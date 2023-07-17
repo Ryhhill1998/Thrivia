b07f4a02f2532496d2c369cf817870f496c0568e
ryhhill1998 <ryhhill1998@outlook.com>
2023-07-17 19:19:39 +0100

    ChatsModel and AllChatsVM now connect with db and chats can be retrieved as required by front end

--- Thrivia/Models/Database/ChatsModel.swift
+++ Thrivia/Models/Database/ChatsModel.swift
@@ -43,24 +43,81 @@ class ChatsModel {
     }
     
     func getUserChats(userId: String, userChatsSetter: @escaping ([Chat]) -> Void) {
-        var userChats: [Chat] = []
-        
         // search db for all chat docs with userId in userIds array
         db.collection("chats").whereField("userIds", arrayContains: userId)
             .getDocuments() { (querySnapshot, err) in
                 if let err = err {
                     print("Error getting documents: \(err)")
                 } else {
+                    // initialise user chats array
+                    var userChats: [Chat] = []
+                    
+                    // initialise chats dispatch group
+                    let chatsDispatchGroup = DispatchGroup()
+                    
                     for chatDoc in querySnapshot!.documents {
                         let chatData = chatDoc.data()
                         let chatId = chatDoc.documentID
                         
-                        if let chat = self.createChatObjectFromData(userId: userId, chatId: chatId, data: chatData) {
-                            userChats.append(chat)
+                        if let userIds = chatData["userIds"] as? [String], let messageIds = chatData["messageIds"] as? [String] {
+                            // get other user data
+                            let otherUserId = userIds[0] == userId ? userIds[1] : userIds[0]
+                            
+                            let docRef = self.db.collection("users").document(otherUserId)
+                            
+                            chatsDispatchGroup.enter()
+                            
+                            docRef.getDocument { (document, error) in
+                                if let otherUserDoc = document, otherUserDoc.exists {
+                                    let otherUserData = otherUserDoc.data()
+                                    if let otherUser = self.createOtherUserObjectFromData(otherUserId: otherUserId, data: otherUserData) {
+                                        // initialise empty messages array
+                                        var messages: [Message] = []
+                                        
+                                        // initialise messages dispatch group
+                                        let messagesDispatchGroup = DispatchGroup()
+                                        
+                                        // get message data
+                                        for messageId in messageIds {
+                                            let docRef = self.db.collection("messages").document(messageId)
+
+                                            messagesDispatchGroup.enter()
+                                            
+                                            docRef.getDocument { (document, error) in
+                                                if let messageDoc = document, messageDoc.exists {
+                                                    let messageData = messageDoc.data()
+
+                                                    if let message = self.createMessageObjectFromData(userId: userId, messageId: messageId, data: messageData) {
+                                                        messages.append(message)
+                                                        messagesDispatchGroup.leave()
+                                                    }
+                                                } else {
+                                                    print("Document does not exist")
+                                                }
+                                            }
+                                        }
+                                        
+                                        messagesDispatchGroup.notify(queue: .main) {
+                                            // sort messages
+                                            messages.sort {
+                                                $0.timestamp < $1.timestamp
+                                            }
+                                            let chat = Chat(id: chatId, otherUser: otherUser, messages: messages)
+                                            userChats.append(chat)
+                                            chatsDispatchGroup.leave()
+                                        }
+                                        
+                                    }
+                                } else {
+                                    print("Document does not exist")
+                                }
+                            }
                         }
                     }
                     
-                    userChatsSetter(userChats)
+                    chatsDispatchGroup.notify(queue: .main) {
+                        userChatsSetter(userChats)
+                    }
                 }
             }
     }
@@ -69,7 +126,7 @@ class ChatsModel {
         var otherUser: OtherUser?
         
         if let username = data?["username"] as? String,
-            let iconColour = data?["iconColour"] as? String {
+           let iconColour = data?["iconColour"] as? String {
             otherUser = OtherUser(id: otherUserId, username: username, iconColour: Color(iconColour))
         }
         
@@ -88,50 +145,4 @@ class ChatsModel {
         return message
     }
     
-    private func createChatObjectFromData(userId: String, chatId: String, data: [String: Any]) -> Chat? {
-        var chat: Chat?
-        
-        var otherUser: OtherUser?
-        
-        if let userIds = data["userIds"] as? [String], let messageIds = data["messageIds"] as? [String] {
-            // get other user data
-            let otherUserId = userIds[0] == userId ? userIds[1] : userIds[0]
-            
-            let docRef = self.db.collection("users").document(otherUserId)
-
-            docRef.getDocument { (document, error) in
-                if let otherUserDoc = document, otherUserDoc.exists {
-                    let otherUserData = otherUserDoc.data()
-                    otherUser = self.createOtherUserObjectFromData(otherUserId: otherUserId, data: otherUserData)
-                } else {
-                    print("Document does not exist")
-                }
-            }
-            
-            var messages: [Message] = []
-            
-            // get message data
-            for messageId in messageIds {
-                let docRef = self.db.collection("messages").document(messageId)
-
-                docRef.getDocument { (document, error) in
-                    if let messageDoc = document, messageDoc.exists {
-                        let messageData = messageDoc.data()
-                        
-                        if let message = self.createMessageObjectFromData(userId: userId, messageId: messageId, data: messageData) {
-                            messages.append(message)
-                        }
-                    } else {
-                        print("Document does not exist")
-                    }
-                }
-            }
-            
-            if let unwrappedOtherUser = otherUser {
-                chat = Chat(id: chatId, otherUser: unwrappedOtherUser, messages: messages)
-            }
-        }
-        
-        return chat
-    }
 }
