ffb16afde78ee2cacd39add6ed690dd624121829
ryhhill1998 <ryhhill1998@outlook.com>
2023-07-22 09:53:08 +0100

    Added password validation on sign up form using border colours

--- Thrivia/Views/TabNavigation/Authentication/Register/RegisterScreen.swift
+++ Thrivia/Views/TabNavigation/Authentication/Register/RegisterScreen.swift
@@ -16,6 +16,23 @@ struct RegisterScreen: View {
     @State var usernameFieldField: String = ""
     @State var passwordFieldText: String = ""
     @State var confirmPasswordFieldText: String = ""
+    @State var showPassword = false
+    
+    @State var confirmPasswordBorderColour = Color.white
+    
+    func comparePasswords(password1: String, password2: String) {
+        var colour: Color
+        
+        if password1.isEmpty || password2.isEmpty {
+            colour = .white
+        } else if password1 == password2 {
+            colour = .green
+        } else {
+            colour = .red
+        }
+        
+        confirmPasswordBorderColour = colour
+    }
     
     func registerClicked() {
         if emailFieldText.isEmpty || usernameFieldField.isEmpty || passwordFieldText.isEmpty || confirmPasswordFieldText.isEmpty { return }
@@ -52,25 +69,83 @@ struct RegisterScreen: View {
                     .foregroundColor(Color("Black"))
                     .padding(.horizontal)
                 
-                TextField("Password", text: $passwordFieldText)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
-                    .background(.white)
+                if !showPassword {
+                    HStack {
+                        SecureField("Password", text: $passwordFieldText)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .font(.custom("Montserrat", size: 18))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("Black"))
+                            .onChange(of: passwordFieldText) { comparePasswords(password1: $0, password2: confirmPasswordFieldText) }
+                        
+                        Button {
+                            showPassword = true
+                        } label: {
+                            Image(systemName: "eye.fill")
+                        }
+                        .padding(.horizontal, 20)
+                        .foregroundColor(Color("Green"))
+                        
+                    }
+                    .background(Color("White"))
                     .cornerRadius(10)
-                    .font(.custom("Montserrat", size: 18))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color("Black"))
                     .padding(.horizontal)
+                } else {
+                    HStack {
+                        TextField("Password", text: $passwordFieldText)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .font(.custom("Montserrat", size: 18))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("Black"))
+                            .onChange(of: passwordFieldText) { comparePasswords(password1: $0, password2: confirmPasswordFieldText) }
+                        
+                        Button {
+                            showPassword = false
+                        } label: {
+                            Image(systemName: "eye.slash.fill")
+                        }
+                        .padding(.horizontal, 20)
+                        .foregroundColor(Color("Green"))
+                        
+                    }
+                    .background(Color("White"))
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                }
                 
-                TextField("Confirm password", text: $confirmPasswordFieldText)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
-                    .background(.white)
-                    .cornerRadius(10)
-                    .font(.custom("Montserrat", size: 18))
-                    .fontWeight(.medium)
-                    .foregroundColor(Color("Black"))
-                    .padding(.horizontal)
+                if !showPassword {
+                    SecureField("Confirm password", text: $confirmPasswordFieldText)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .font(.custom("Montserrat", size: 18))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Black"))
+                        .background(Color("White"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(confirmPasswordBorderColour, lineWidth: 3)
+                        )
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                        .onChange(of: confirmPasswordFieldText) { comparePasswords(password1: $0, password2: passwordFieldText) }
+                } else {
+                    TextField("Confirm password", text: $confirmPasswordFieldText)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 10)
+                        .font(.custom("Montserrat", size: 18))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Black"))
+                        .background(Color("White"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(confirmPasswordBorderColour, lineWidth: 3)
+                        )
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                        .onChange(of: confirmPasswordFieldText) { comparePasswords(password1: $0, password2: passwordFieldText) }
+                }
                 
                 ActionButton(text: "Register", fontColour: .white, backgroundColour: Color("Green"), action: registerClicked)
                 
